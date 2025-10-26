b5f9316199d318743c0b231cf6fa2a7e64963c91
Alex Etidit <144605321+etiditalex@users.noreply.github.com>
2025-10-26 21:16:30 +0300

    Updates

--- assets/InstructionsModal-U8laj0yX.js
+++ assets/InstructionsModal-U8laj0yX.js
@@ -1,4 +1,4 @@
-import{j as e}from"./three-vendor-oqmeVQoY.js";import{g as x}from"./gestureDetection-RmCIMLIr.js";import{m as r}from"./animation-vendor-1fEUMvyv.js";import{c as n,X as h,a as i}from"./index-CIw8cbtC.js";import{I as p,C as y,V as u}from"./SignLanguageMode-tPbHk8c8.js";import"./react-vendor-CXK_3RTl.js";import"./chart-vendor-CflBAgXr.js";/**
+import{j as e}from"./three-vendor-oqmeVQoY.js";import{g as x}from"./gestureDetection-RmCIMLIr.js";import{m as r}from"./animation-vendor-1fEUMvyv.js";import{c as n,X as h,a as i}from"./index-DgIhXTJC.js";import{I as p,C as y,V as u}from"./SignLanguageMode-Bq70mLqY.js";import"./react-vendor-CXK_3RTl.js";import"./chart-vendor-CflBAgXr.js";/**
  * @license lucide-react v0.303.0 - ISC
  *
  * This source code is licensed under the ISC license.
