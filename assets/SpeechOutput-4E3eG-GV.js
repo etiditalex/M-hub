a1172be7371e44ca0b6eb94dcb8219e93872357f
Alex Etidit <144605321+etiditalex@users.noreply.github.com>
2025-10-26 14:41:43 +0300

    Updates

--- assets/SpeechOutput-4E3eG-GV.js
+++ assets/SpeechOutput-4E3eG-GV.js
@@ -1,4 +1,4 @@
-import{j as e}from"./three-vendor-oqmeVQoY.js";import{r as c}from"./react-vendor-CXK_3RTl.js";import{m as p}from"./animation-vendor-1fEUMvyv.js";import{V as v,a as k}from"./SignLanguageMode-Dk1dREMr.js";import{c as y}from"./index-jMHYcBJt.js";import"./chart-vendor-CflBAgXr.js";/**
+import{j as e}from"./three-vendor-oqmeVQoY.js";import{r as c}from"./react-vendor-CXK_3RTl.js";import{m as p}from"./animation-vendor-1fEUMvyv.js";import{V as v,a as k}from"./SignLanguageMode-DUjo7D8Z.js";import{c as y}from"./index-DojJYaOq.js";import"./chart-vendor-CflBAgXr.js";/**
  * @license lucide-react v0.303.0 - ISC
  *
  * This source code is licensed under the ISC license.
